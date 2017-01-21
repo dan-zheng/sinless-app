cc0b034a69b45b25c7bba780488801fde5e5d718
nisargkolhe <nisargkolhe@gmail.com>
2017-01-21 00:02:19 -0500

    Added IntroActivity file

---
+++ Android/MyApplication3/app/src/main/java/org/octocats/sinless/MainIntroActivity.java
@@ -0,0 +1,16 @@
+package org.octocats.sinless;
+
+import android.os.Bundle;
+
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+
+/**
+ * Created by nisarg on 21/1/17.
+ */
+
+public class MainIntroActivity extends IntroActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
