3a65e3a7c1194ecc23bdc0f825760aa487d6f3fb
UtkarshJain98 <utkjain@gmail.com>
2017-01-21 03:03:38 -0500

    recycler view

---
+++ Android/MyApplication3/app/src/main/java/org/octocats/sinless/Timeline.java
@@ -0,0 +1,33 @@
+package org.octocats.sinless;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+
+import com.andexert.expandablelayout.library.ExpandableLayoutListView;
+
+import static org.octocats.sinless.R.layout.activity_timeline;
+
+/**
+ * Created by utkarsh on 21/1/17.
+ */
+
+public class Timeline extends AppCompatActivity{
+
+    private final String[] array = {"Hello", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_timeline);
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.view_content, R.id.view_header, array);
+        final ExpandableLayoutListView expandableLayoutListView = (ExpandableLayoutListView) findViewById(R.id.listview);
+
+        expandableLayoutListView.setAdapter(arrayAdapter);
+    }
+
+
+}
