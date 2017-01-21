3fbe2b89dd9f42e329080dafeb884deae4a8b639
nisargkolhe <nisargkolhe@gmail.com>
2017-01-21 01:00:59 -0500

    Created check_layout

--- Android/MyApplication3/app/src/main/java/org/octocats/sinless/MainIntroActivity.java
+++ Android/MyApplication3/app/src/main/java/org/octocats/sinless/MainIntroActivity.java
@@ -3,6 +3,7 @@ package org.octocats.sinless;
 import android.os.Bundle;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
 /**
  * Created by nisarg on 21/1/17.
@@ -12,5 +13,16 @@ public class MainIntroActivity extends IntroActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.colorPrimary)
+                .backgroundDark(R.color.colorPrimaryDark)
+                .fragment(R.layout.check_layout, R.style.AppTheme)
+                .build());
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.colorAccent)
+                .backgroundDark(R.color.colorAccent)
+                .fragment(R.layout.check_layout, R.style.AppTheme)
+                .build());
     }
 }
