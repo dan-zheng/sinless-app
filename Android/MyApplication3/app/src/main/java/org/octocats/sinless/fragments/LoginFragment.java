ad3f530bd19693aa67caf52771bb14089965bb51
nisargkolhe <nisargkolhe@gmail.com>
2017-01-21 03:42:36 -0500

    Login working

--- Android/MyApplication3/app/src/main/java/org/octocats/sinless/fragments/LoginFragment.java
+++ Android/MyApplication3/app/src/main/java/org/octocats/sinless/fragments/LoginFragment.java
@@ -2,15 +2,27 @@ package org.octocats.sinless.fragments;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.heinrichreimersoftware.materialintro.app.SlideFragment;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.octocats.sinless.R;
+
+import java.util.HashMap;
+
+import cz.msebera.android.httpclient.Header;
 
 /**
  * Created by nisarg on 21/1/17.
@@ -18,10 +30,9 @@ import org.octocats.sinless.R;
 
 public class LoginFragment extends SlideFragment {
 
+    private CoordinatorLayout coordinatorLayout;
     private EditText fakeUsername;
-
     private EditText fakePassword;
-
     private Button fakeLogin;
 
     private boolean loggedIn = false;
@@ -43,6 +54,10 @@ public class LoginFragment extends SlideFragment {
         // Required empty public constructor
     }
 
+    private final String URL = "http://pal-nat186-94-246.itap.purdue.edu:3000/api";
+
+    private final String TAG = "LoginFragment";
+
     public static LoginFragment newInstance() {
         return new LoginFragment();
     }
@@ -53,6 +68,9 @@ public class LoginFragment extends SlideFragment {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_login, container, false);
 
+        final AsyncHttpClient client = new AsyncHttpClient();
+
+        coordinatorLayout = (CoordinatorLayout) root.findViewById(R.id.coordinatorLayout);
         fakeUsername = (EditText) root.findViewById(R.id.email);
         fakePassword = (EditText) root.findViewById(R.id.password);
         fakeLogin = (Button) root.findViewById(R.id.fakeLogin);
@@ -69,7 +87,34 @@ public class LoginFragment extends SlideFragment {
                 fakeLogin.setEnabled(false);
                 fakeLogin.setText("Loading");
 
-                loginHandler.postDelayed(loginRunnable, 2000);
+                HashMap<String, String> paramMap = new HashMap<String, String>();
+                paramMap.put("email", fakeUsername.getText().toString());
+                paramMap.put("password", fakePassword.getText().toString());
+
+                RequestParams params = new RequestParams(paramMap);
+
+                client.post(getContext(), URL + "/login", params, new JsonHttpResponseHandler(){
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        Log.e(TAG, response.toString());
+                        loginRunnable.run();
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, Throwable t, JSONObject response) {
+                        Log.e(TAG, response.toString());
+                        fakeLogin.setText("Login");
+                        fakeUsername.setEnabled(true);
+                        fakePassword.setEnabled(true);
+                        fakeLogin.setEnabled(true);
+                        try {
+                            Toast.makeText(getContext(), response.getString("message"), Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                //loginHandler.postDelayed(loginRunnable, 2000);
             }
         });
 
