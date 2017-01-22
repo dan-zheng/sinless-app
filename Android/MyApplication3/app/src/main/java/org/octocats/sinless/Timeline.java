20b94525f4d5523590ba3444f9f5a0124e96ad17
nisargkolhe <nisargkolhe@gmail.com>
2017-01-21 23:51:26 -0500

    moved to heroku

--- Android/MyApplication3/app/src/main/java/org/octocats/sinless/Timeline.java
+++ Android/MyApplication3/app/src/main/java/org/octocats/sinless/Timeline.java
@@ -44,7 +44,7 @@ public class Timeline extends AppCompatActivity{
 
     private String TAG = "Timeline";
 
-    private final String URL = "http://pal-nat186-94-246.itap.purdue.edu:3000/api";
+    public final String URL = "http://sinless.herokuapp.com/api";
 
     private HashMap<String, ArrayList<Action>> dataMap = new HashMap<>();
     private ArrayList<String> dates = new ArrayList<>();
